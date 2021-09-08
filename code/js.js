7d2a7921cae90822e285cfc9d6eb3fb684a18667
Darina Kopenkova <darisha_92@mail.ru>
2021-09-08 13:14:53 +0300

    add imgages with JS

--- code/js.js
+++ code/js.js
@@ -12,9 +12,21 @@ next.addEventListener("click", function () {
 
 prev.addEventListener("click", function () {
   let scrollLeft = slides.scrollLeft;
-  console.log(scrollLeft);
   slides.scrollLeft -= 500;
   if (slides.scrollLeft === scrollLeft) {
     slides.scrollLeft = 1500;
   }
 });
+
+let path = "../Img";
+let images = ["circus2.jpeg", "circus3.png", "circus4.png"];
+
+let img = document.createElement("img");
+
+images.forEach((image) => {
+  img.src = path + "/" + image;
+  img.alt = image;
+  slides.innerHTML += img.outerHTML;
+});
+
+img.src = "";
